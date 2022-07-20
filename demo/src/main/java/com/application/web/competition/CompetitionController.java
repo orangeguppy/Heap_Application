e6be13419e3f12a56944c96be73c19e87b662676
sarahcwy <106381214+sarahcwy@users.noreply.github.com>
2022-07-21 01:07:22 +0800

    very minor changes!!

--- demo/src/main/java/com/application/web/competition/CompetitionController.java
+++ demo/src/main/java/com/application/web/competition/CompetitionController.java
@@ -1,7 +1,21 @@
 package com.application.web.competition;
 
+import com.application.database.competition.repository.CompetitionRepository;
+import com.application.database.competition.entity.Competition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/competitions")
 public class CompetitionController {
+    @Autowired
+    CompetitionRepository competitionRepository;
+
+
 }
