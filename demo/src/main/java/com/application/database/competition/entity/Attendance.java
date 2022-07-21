15b803779d8fd13ae18def963738e9a9cad069c6
sarahcwy <106381214+sarahcwy@users.noreply.github.com>
2022-07-21 17:10:22 +0800

    Update Attendance.java

--- demo/src/main/java/com/application/database/competition/entity/Attendance.java
+++ demo/src/main/java/com/application/database/competition/entity/Attendance.java
@@ -14,6 +14,8 @@ public class Attendance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int attendanceID;
+
+    private int UID; // of user
     private boolean attendance;
 
     public Attendance () {
