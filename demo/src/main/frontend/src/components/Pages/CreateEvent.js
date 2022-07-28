6c4b37db4578fde9cf1a438802a3c79ea810f4a2
sarahcwy <106381214+sarahcwy@users.noreply.github.com>
2022-07-28 21:31:56 +0800

    create event testing

--- demo/src/main/frontend/src/components/Pages/CreateEvent.js
+++ demo/src/main/frontend/src/components/Pages/CreateEvent.js
@@ -1,47 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../CreateEvent.css';
 import {Link} from 'react-router-dom';
 
 function CreateEvent() {
+const [eventName, setEventName] = useState('');
+const [eventDescription, setEventDescription] = useState('');
+const [eventDescription, setEventDescription] = useState('Competition');
+const [eventStartDate, setEventStartDate] = useState(); //tbc
+const [eventEndDate, setEventEndDate] = useState(); //tbc
+const [eventLocation, setEventLocation] = useState('');
+const [maxCapacity, setMaxCapacity] = useState(0);
+const [eventRoles, setEventRoles] = useState('');
+const [signupsStartDate, setSignupsStartDate] = useState(); //tbc
+const [signupsEndDate, setSignupsEndDate] = useState(); //tbc
+
+
     return(
         <div>
             <h1> Create New Event </h1>
             <h2> Event Details </h2>
-            <h3> Event Name </h3>
-            <input type="text" placeholder="Enter event name" />
-            <h3> Event Type </h3>
-            <select>
-                  <option value="HTML">Select an Event Type</option>
-                  <option value="Competition">Competition</option>
-                  <option value="Seminar">Seminar</option>
-                  <option value="Others">Others</option>
+            <form>
+            <label> Event Name </label>
+            <input type="text" placeholder="Enter event name"
+            required
+            value={eventName}
+            onChange = {(e) => setEventName(e.target.value)}
+            />
+            <label> Event Description </label>
+            <textarea
+            required
+            value={eventDescription}
+            onChange = {(e) => setEventDescription(e.target.value)}
+            ></textarea>
+            <label> Event Type </label>
+            <select
+            value = {eventType}
+            onChange = {(e) => setEventType(e.target.value)}
+            >
+                <option value="HTML">Select an Event Type</option>
+                <option value="Competition">Competition</option>
+                <option value="Seminar">Seminar</option>
+                <option value="Others">Others</option>
             </select>
-            <h3> Event Start Date </h3>
-            <input type="datetime-local" />
-            <h3> Event End Date </h3>
-            <input type="datetime-local" />
-            <h3> Location </h3>
-            // tbc
-            <h3> Capacity </h3>
-            <input type="number" placeholder="Enter a number" />
+            <label> Event Start Date </label>
+            <input type="datetime-local"
+            value = {eventStartDate}
+            onChange = {(e) => setEventStartDate(e.target.value)}
+            />
+            <label> Event End Date </label>
+            <input type="datetime-local"
+            value = {eventEndDate}
+            onChange = {(e) => setEventEndDate(e.target.value)}
+            />
+            <label> Location </label>
+            <input type="text" placeholder="Enter event location"
+            required
+            value={eventLocation}
+            onChange = {(e) => setEventLocation(e.target.value)}
+            />
+            <label> Capacity </label>
+            <input type="number" placeholder="Enter a number"
+            value={maxCapacity}
+            onChange = {(e) => setMaxCapacity(e.target.value)}
+            />
             <h2> Participants </h2>
-            <h3> Roles </h3>
-            <select>
-                  <option value="HTML">Select a Role</option>
-                  <option value="Host">Host</option>
-                  <option value="Judge">Judge</option>
-                  <option value="Participant">Participant</option>
-                  <option value="Custom">Custom</option>
+            <label> Roles Available</label>
+            <select
+            value = {eventRoles}
+            onChange = {(e) => setEventRoles(e.target.value)} //how to choose multiple?
+            >
+                 <option value="HTML">Select a Role</option>
+                 <option value="Host">Host</option>
+                 <option value="Judge">Judge</option>
+                 <option value="Participant">Participant</option>
+                 <option value="Custom">Custom</option>
             </select>
             <h2> Signups </h2>
-            <h3> Signups Start Date </h3>
-            <input type="datetime-local" />
-            <h3> Signups End Date </h3>
-            <input type="datetime-local" />
+            <label> Signups Start Date </label>
+            <input type="datetime-local"
+            value = {signupsStartDate}
+            onChange = {(e) => setSignupsStartDate(e.target.value)}
+            />
+            <label> Signups End Date </label>
+            <input type="datetime-local"
+            value = {signupsEndDate}
+            onChange = {(e) => setSignupsEndDate(e.target.value)}
+            />
             <h2> Alerts </h2>
-            //tbc
 
-
+            </form>
             <button>Click</button>
         </div>
     )
